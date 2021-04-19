2f61b1a7efb03a5c3781d19993ecf50fd878e6a7
pasinduBhasura <srmpasindubhasura@gmail.com>
2021-04-19 19:02:07 +0530

    fixed some issues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,7 @@ function Header(){
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Branches <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/branches">Branches <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/add">Create Branch</a>
